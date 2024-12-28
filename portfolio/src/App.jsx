425d37732c0429ded853aedd531c182b14f24407
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2024-12-28 23:13:05 +0600

    Home page design done without media querry

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
 import Header from './components/Header'
+import Home from './components/Home/Home'
 function App() {
  
 
   return (
     <>
      <Header/>
+     <main className='main'>
+
+     <Home/>
+     </main>
+    
         
     </>
   )
---
+++ portfolio/src/components/Home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/naeem-khandakar-845240216?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="home__soical-icon" target='_blank'>
+        <i class="uil uil-linkedin"></i>
+        </a>
+        <a href="https://github.com/Naeem-khndkar55" className="home__soical-icon" target='_blank'>
+        <i class="uil uil-github-alt"></i>
+        </a>
+        <a href="https://www.facebook.com/naeem.khandakar.75/" className="home__soical-icon" target='_blank'>
+        <i class="uil uil-facebook"></i>
+        </a>
+        
+    </div>
+  )
+}
+
+export default Social
