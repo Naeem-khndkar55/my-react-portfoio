8a58f4d0fa5f55ca3f2e18e0a8c600dbf9d075c5
Naeem-khndkar55 <naeemkhandakar5@gmail.com>
2025-01-12 20:26:41 +0600

    scalaton problem fixed and add some css

--- portfolio/src/components/qualifications/Qualifications.jsx
+++ portfolio/src/components/qualifications/Qualifications.jsx
@@ -9,124 +9,121 @@ const Qualifications = () => {
       <div className="qualifications__container container">
         <div className="qualifications__tabs">
           <div className="qualifications__button qualifications__active button--flex">
-            <i className="uil uil-graduation-cap qualifications__icon">
-              Education
-            </i>
+            <i className="uil uil-graduation-cap qualifications__icon"></i>
+            Education
           </div>
           <div className="qualifications__button button--flex">
-            <i className="uil uil-briefcase-alt qualifications__icon">
-              Experience
-            </i>
+            <i className="uil uil-briefcase-alt qualifications__icon"></i>
+            Experience
           </div>
         </div>
+
         <div className="qualifications__sections">
           <div className="qualifications__content qualifications__content-active">
             <div className="qualifications__data">
               <div>
-                <h3 className="qualifiactions__title">Graduation</h3>
+                <h3 className="qualifications__title">Graduation</h3>
                 <span className="qualifications__subtitle">
                   Comilla University
                 </span>
-                <div className="qualifiations__calendar">
-                  <i className="uil uil-calendar-alt">2020-2025</i>
+                <div className="qualifications__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2020-2025
                 </div>
               </div>
               <div>
-                <span className="qualifiacions__rounder"></span>
+                <span className="qualifications__rounder"></span>
                 <span className="qualifications__line"></span>
               </div>
             </div>
             <div className="qualifications__data">
               <div></div>
               <div>
-                <span className="qualifiacions__rounder"></span>
+                <span className="qualifications__rounder"></span>
                 <span className="qualifications__line"></span>
               </div>
               <div>
-                <h3 className="qualifiactions__title">Web Development</h3>
+                <h3 className="qualifications__title">Web Development</h3>
                 <span className="qualifications__subtitle">
                   BongoDev & YouTube
                 </span>
-                <div className="qualifiations__calendar">
-                  <i className="uil uil-calendar-alt">2022-Present</i>
+                <div className="qualifications__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2022-Present
                 </div>
               </div>
             </div>
             <div className="qualifications__data">
               <div>
-                <h3 className="qualifiactions__title">Coding</h3>
+                <h3 className="qualifications__title">Coding</h3>
                 <span className="qualifications__subtitle">Self & Youtube</span>
-                <div className="qualifiations__calendar">
-                  <i className="uil uil-calendar-alt">2020-present</i>
+                <div className="qualifications__calendar">
+                  <i className="uil uil-calendar-alt"></i>2020-present
                 </div>
               </div>
               <div>
-                <span className="qualifiacions__rounder"></span>
+                <span className="qualifications__rounder"></span>
                 <span className="qualifications__line"></span>
               </div>
             </div>
             <div className="qualifications__data">
               <div></div>
               <div>
-                <span className="qualifiacions__rounder"></span>
+                <span className="qualifications__rounder"></span>
                 <span className="qualifications__line"></span>
               </div>
               <div>
-                <h3 className="qualifiactions__title">
-                  {" "}
-                  Communucation & English
-                </h3>
+                <h3 className="qualifications__title">Communucation</h3>
                 <span className="qualifications__subtitle">
-                  Self, Multiple Club & Org.
+                  Self, Multiple Club & Orgs
                 </span>
-                <div className="qualifiations__calendar">
-                  <i className="uil uil-calendar-alt">2021-Present</i>
+                <div className="qualifications__calendar">
+                  <i className="uil uil-calendar-alt"></i>2021-Present
                 </div>
               </div>
             </div>
           </div>
+
           <div className="qualifications__content">
             <div className="qualifications__data">
               <div>
-                <h3 className="qualifiactions__title">MERN Stack</h3>
+                <h3 className="qualifications__title">MERN Stack</h3>
                 <span className="qualifications__subtitle">
                   University projects & Practice
                 </span>
-                <div className="qualifiations__calendar">
-                  <i className="uil uil-calendar-alt">2022-present</i>
+                <div className="qualifications__calendar">
+                  <i className="uil uil-calendar-alt"></i>2022-present
                 </div>
               </div>
               <div>
-                <span className="qualifiacions__rounder"></span>
+                <span className="qualifications__rounder"></span>
                 <span className="qualifications__line"></span>
               </div>
             </div>
             <div className="qualifications__data">
               <div></div>
               <div>
-                <span className="qualifiacions__rounder"></span>
+                <span className="qualifications__rounder"></span>
                 <span className="qualifications__line"></span>
               </div>
               <div>
-                <h3 className="qualifiactions__title">Team Work</h3>
+                <h3 className="qualifications__title">Team Work</h3>
                 <span className="qualifications__subtitle">
                   Clubing & Events management
                 </span>
-                <div className="qualifiations__calendar">
+                <div className="qualifications__calendar">
                   <i className="uil uil-calendar-alt">2023-Present</i>
                 </div>
               </div>
             </div>
             <div className="qualifications__data">
               <div>
-                <h3 className="qualifiactions__title">Mentor</h3>
+                <h3 className="qualifications__title">Mentor</h3>
                 <span className="qualifications__subtitle">CoderPals</span>
-                <div className="qualifiations__calendar">
-                  <i className="uil uil-calendar-alt">2025-present</i>
+                <div className="qualifications__calendar">
+                  <i className="uil uil-calendar-alt"></i>2025-present
                 </div>
               </div>
               <div>
-                <span className="qualifiacions__rounder"></span>
+                <span className="qualifications__rounder"></span>
                 <span className="qualifications__line"></span>
               </div>
             </div>
